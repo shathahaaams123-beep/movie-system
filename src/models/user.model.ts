d304921fcb3c1035a2166eb92717594a6c18dea0
HabibaMahmoud-S <habibamahmouud280.com>
2026-08-17 23:13:34 +0400

    feat: add user model, controller, and tsconfig settings

---
+++ src/models/user.model.ts
@@ -0,0 +1,30 @@
+import { Schema,model,Document } from 'mongoose' ;
+
+export interface IUser extends Document{
+
+  name : string ;
+  email : string;
+  passwordHash : string;
+
+  role: 'admin' | 'customer';
+
+  createdAt: Date;
+}
+
+const userSchema=new Schema<IUser>(
+
+  {
+    name: { type: String , required: true , trim: true } ,
+
+    email: { type: String , required: true, unique: true , lowercase: true , trim: true },
+
+    passwordHash: { type: String , required: true },
+
+    role: { type : String,enum: ['admin', 'customer'], default: 'customer'},
+  },
+  {
+
+    timestamps:true,
+  }
+);
+export const User =model<IUser>('User', userSchema);
